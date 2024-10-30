e2a2177280d757a2ba5fe074cfff6398c5fc528a
anniey54 <annie.ye@torontomu.ca>
2024-10-30 17:00:18 -0400

    Created recipe card component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,17 @@
 import react from 'react';
 import toast from 'react-hot-toast';
+import RecipeCard from '@/components/RecipeCard';
 
 export default function Home() {
   return (
-    <div>
+    <div className='flex flex-col'>
       <h1>Welcome to Next.js</h1>
       <h2 className='text-primary'>Hello, world!</h2>
       
       <i className="fa-brands fa-xing"></i>
       <button onClick={() => toast('Hello, world!')}>Click me!</button>
       <input type='text' placeholder='Type something...' />
+      <RecipeCard name='Steak' image={"https://images.unsplash.com/photo-1432139509613-5c4255815697?q=80&w=1885&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"} author='master_chief' rating={4} />
     </div>
   );
 }
