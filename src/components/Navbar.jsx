dde0f70a0c288310af24608ef214a371ec830be3
Anna Ye <annaa.ye@mail.utoronto.ca>
2024-10-31 21:16:25 -0400

    installed NextUI and created navbar when user login for large screen

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,11 @@
 import Link from 'next/link';
 import React from 'react';
+import {Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Button} from "@nextui-org/react";
+
 
 const Navbar = () => {
+  const user = true; // temporary user state
+
   return (
     <div className='flex flex-row flex-nowrap justify-between bg-white w-full min-[850px]:h-20'>
       {/* logo and brand name */}
@@ -11,15 +15,38 @@ const Navbar = () => {
       </div>
       {/* nav links */}
       <div className='flex flex-row gap-6 items-center min-[850px]:pr-[3%] lg:pr-[5%]'>
-        <Link href='/' className='font-normal text-xl'>Home</Link>
-        <Link href='/explore' className='font-normal text-xl'>Explore</Link>
-        <Link href='/contact' className='font-normal text-xl'>Contact</Link>
-        <Link href='/about' className='font-normal text-xl'>About</Link>
-        <Link href='/login'>
-          <button className='flex flex-nowrap gap-2 items-center bg-primary text-white font-normal text-xl px-4 rounded-lg'>
-            Login
-          </button>
-        </Link>
+        <Link href='/' className='font-normal text-xl text-textColor'>Home</Link>
+        <Link href='/explore' className='font-normal text-xl text-textColor'>Explore</Link>
+        <Link href='/contact' className='font-normal text-xl text-textColor'>Contact</Link>
+        <Link href='/about' className='font-normal text-xl text-textColor'>About</Link>
+        {/* login or account button */}
+        {user ? (
+          <Dropdown>
+            <DropdownTrigger>
+              <Button 
+                color='primary'
+                variant='solid'
+                className='flex flex-nowrap gap-2 items-center text-white font-normal text-xl px-4 rounded-lg'
+              >
+                <i class="fa-regular fa-user"></i>
+                Account
+              </Button>
+            </DropdownTrigger>
+            <DropdownMenu
+              color='primary'
+              variant='light'
+            >
+              <DropdownItem key='dashboard' href='/dashboard'><p>My dashboard</p></DropdownItem>
+              <DropdownItem key='create-recipe' href='/create_recipe'><p>Create recipe</p></DropdownItem>
+              <DropdownItem key='logout' href='/logout'><p>Logout</p></DropdownItem>
+            </DropdownMenu>
+          </Dropdown>)
+        : (
+          <Link href='/login'>
+            <button className='flex flex-nowrap gap-2 items-center bg-primary text-white font-normal text-xl px-4 rounded-lg'>
+              Login
+            </button>
+          </Link>)}
       </div>
     </div>
   )
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,12 +2,16 @@ import "@/styles/globals.css";
 import { Toaster } from "react-hot-toast";
 import Navbar from "@/components/Navbar";
 
+import {NextUIProvider} from "@nextui-org/react";
+
 export default function App({ Component, pageProps }) {
   return (
   <>
+    <NextUIProvider>
     <Navbar />
     <Component {...pageProps} />
     <Toaster position="top-center" reverseOrder="false" />
+    </NextUIProvider>
   </>
   );
 }
