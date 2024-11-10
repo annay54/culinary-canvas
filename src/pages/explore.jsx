456a2d5cf7877b69251c1c59180c57844e845869
Anna Ye <annaa.ye@mail.utoronto.ca>
2024-11-10 18:55:10 -0500

    created tag filter

--- src/pages/explore.jsx
+++ src/pages/explore.jsx
@@ -1,6 +1,10 @@
 import React from "react";
+import {Select, SelectItem} from "@nextui-org/react";
 
 const Explore = () => {
+  const tags = ["tag1", "tag2", "tag3", "tag4", "tag5", "tag6", "tag7", "tag8", "tag9", "tag10"];
+  const [values, setValues] = React.useState([]);
+
   return (
     <div className="flex flex-row ">
       {/* Filter column */}
@@ -34,6 +38,28 @@ const Explore = () => {
                 <option value={4}>4</option>
                 <option value={5}>5</option>
               </select>
+            </div>
+          </div>
+          <hr></hr>
+          <div>
+            <h3>Tags</h3>
+            <div className="m-2">
+              <Select
+                items={tags}
+                placeholder="Select tags"
+                variant="flat"
+                isMultiline={true}
+                selectionMode="multiple"
+                className="max-w-xs"
+                selectedKeys={values}
+                onSelectionChange={setValues}
+                style={{ color: "#A55913", border: "2px solid #A55913" }}
+                selectorIcon={<i class="fa-solid fa-chevron-down"></i>}
+              >
+                {tags.map((tag) => (
+                  <SelectItem key={tag}>{tag}</SelectItem>
+                ))}
+              </Select>
             </div>
           </div>
           <hr></hr>
