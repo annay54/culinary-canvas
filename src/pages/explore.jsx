607f50a5a5bfa94f79188173959675f4811f2791
Anna Ye <annaa.ye@mail.utoronto.ca>
2024-11-10 19:31:41 -0500

    remove unnecessary div element in filter section

--- src/pages/explore.jsx
+++ src/pages/explore.jsx
@@ -9,61 +9,64 @@ const Explore = () => {
   return (
     <div className="flex flex-row ">
       {/* Filter column */}
-      <div className="bg-primary w-1/3 h-96">
-        <div className="flex flex-col text-white gap-2 mx-4 my-2">
-          <div className="flex flex-row items-center gap-2 mt-2">
-            <i className="fa-solid fa-sliders text-2xl"></i>
-            <h2>Filters</h2>
-          </div>
-          <hr></hr>
-          <div>
-            <h3>Rating</h3>
-            <div className="flex flex-row items-center gap-2 m-2">
-              <p>Minimum</p>
-              <select className="text-secondary text-base w-12 h-8 p-1 rounded-xl border-2">
-                <option value={null}>-</option>
-                <option value={1}>1</option>
-                <option value={2}>2</option>
-                <option value={3}>3</option>
-                <option value={4}>4</option>
-                <option value={5}>5</option>
-              </select>
-            </div>
-            <div className="flex flex-row items-center gap-2 m-2">
-              <p>Maximum</p>
-              <select className="text-secondary text-base w-12 h-8 p-1 rounded-xl border-2">
-              <option value={null}>-</option>
-                <option value={1}>1</option>
-                <option value={2}>2</option>
-                <option value={3}>3</option>
-                <option value={4}>4</option>
-                <option value={5}>5</option>
-              </select>
-            </div>
-          </div>
-          <hr></hr>
-          <div>
-            <h3>Tags</h3>
-            <div className="m-2">
-              <Select
-                items={tags}
-                placeholder="Select tags"
-                variant="flat"
-                isMultiline={true}
-                selectionMode="multiple"
-                className="max-w-xs"
-                selectedKeys={values}
-                onSelectionChange={setValues}
-                style={{ color: "#A55913", border: "2px solid #A55913" }}
-              >
-                {tags.map((tag) => (
-                  <SelectItem key={tag}>{tag}</SelectItem>
-                ))}
-              </Select>
-            </div>
-          </div>
-          <hr></hr>
+      <div className="bg-primary w-1/3 h-fit flex flex-col text-white gap-2 px-4 py-2">
+        <div className="flex flex-row items-center gap-2 mt-2">
+          <i className="fa-solid fa-sliders text-2xl"></i>
+          <h2>Filters</h2>
         </div>
+        <hr></hr>
+        <div>
+          <h3>Rating</h3>
+          <div className="flex flex-row items-center gap-2 m-2">
+            <p>Minimum</p>
+            <select className="text-secondary text-base w-12 h-8 p-1 rounded-xl border-2">
+              <option value={null}>-</option>
+              <option value={1}>1</option>
+              <option value={2}>2</option>
+              <option value={3}>3</option>
+              <option value={4}>4</option>
+              <option value={5}>5</option>
+            </select>
+          </div>
+          <div className="flex flex-row items-center gap-2 m-2">
+            <p>Maximum</p>
+            <select className="text-secondary text-base w-12 h-8 p-1 rounded-xl border-2">
+            <option value={null}>-</option>
+              <option value={1}>1</option>
+              <option value={2}>2</option>
+              <option value={3}>3</option>
+              <option value={4}>4</option>
+              <option value={5}>5</option>
+            </select>
+          </div>
+        </div>
+        <hr></hr>
+        <div>
+          <h3>Tags</h3>
+          <div className="m-2">
+            <Select
+              items={tags}
+              placeholder="Select tags"
+              variant="flat"
+              isMultiline={true}
+              selectionMode="multiple"
+              className="max-w-xs"
+              selectedKeys={values}
+              onSelectionChange={setValues}
+              style={{ color: "#A55913", border: "2px solid #A55913" }}
+            >
+              {tags.map((tag) => (
+                <SelectItem key={tag}>{tag}</SelectItem>
+              ))}
+            </Select>
+          </div>
+        </div>
+        <hr></hr>
+        <div className="flex flex-row items-center gap-2 my-2">
+          <i className="fa-solid fa-sort text-2xl"></i>
+          <h2>Sort</h2>
+        </div>
+        <hr></hr>
       </div>
       {/* Search and result column */}
       <div className="w-2/3 h-auto">
