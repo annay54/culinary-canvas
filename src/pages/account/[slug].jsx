2f6b75d968b42e5811e373b55177dc21ed94bab5
Anna Ye <annaa.ye@mail.utoronto.ca>
2025-01-14 01:17:41 -0500

    reorganize code so that only content of selected section is shown

--- src/pages/account/[slug].jsx
+++ src/pages/account/[slug].jsx
@@ -21,40 +21,8 @@ export default function ({ slug }) {
     )
   }
 
-  return (
-    <div className="flex flex-col md:flex-row w-full h-fit">
-      {/* Side navigation bar for computer screen size */}
-      <div className="hidden md:flex flex-col py-2 min-w-48 w-1/5 bg-primary text-white">
-        {navSection.map((section, index) => (
-          <>
-            <div className={`flex flex-row items-center gap-2 px-6 py-3 hover:bg-secondary hover:cursor-pointer ${selectSection.name === section.name ? "bg-secondary" : ""}`} onClick={() => setSelectSection(section.name)}>
-              <i className={`fa-solid ${section.icon} text-lg`}></i>
-              <h3 className="text-xl">{section.name}</h3>
-            </div>
-            <hr className="border-white w-11/12 self-center"></hr>
-          </>
-        ))}
-      </div>
-      {/* Top navigation bar for mobile screen size */}
-      <div className="md:hidden flex flex-row justify-between items-center bg-secondary text-white w-full h-12 px-6">
-        <div className={`flex flex-row items-center gap-2`}>
-          <i className={`fa-solid ${selectSection.icon} text-md`}></i>
-          <h3 className="text-lg">{selectSection.name}</h3>
-        </div>
-        <i className={`fa-solid ${showSection ? "fa-chevron-up" : "fa-chevron-down"} text-lg hover:cursor-pointer`} onClick={() => setShowSection(showSection == false)}></i>
-      </div>
-      <div className={`md:hidden flex flex-col bg-primary text-white w-full h-fit ${showSection ? "flex" : "hidden"}`}>
-        {navSection.map((section, index) => (
-          <>
-            <div className={`flex flex-row items-center gap-2 px-6 py-3 hover:bg-secondary hover:cursor-pointer`} onClick={() => {setSelectSection(section); setShowSection(false)}}>
-              <i className={`fa-solid ${section.icon} text-md`}></i>
-              <h3 className="text-lg">{section.name}</h3>
-            </div>
-            <hr className="border-white w-11/12 self-center"></hr>
-          </>
-        ))}
-      </div>
-      {/* Content displayed */}
+  const Profile = () => {
+    return (
       <div className="flex flex-col gap-5 p-10 xl:px-20 w-full">
         <div className="flex flex-row items-center gap-4">
           <Avatar className="w-20 h-20" />
@@ -63,7 +31,7 @@ export default function ({ slug }) {
             <h3 className="text-xl font-normal">Toronto, ON</h3>
           </div>
         </div>
-        <div className="flex flex-col min-[600px]:flex-row gap-5 min-[600px]:gap-3 justify-evenly w-full">
+        <div className="flex flex-col min-[600px]:flex-row gap-5 min-[600px]:gap-3 justify-evenly w-full my-3">
           <div className="flex flex-col gap-1 justify-center items-center bg-white p-5 lg:w-1/4 min-[850px]:px-7 min-w-36 min-h-36">
             <div className="flex flex-row items-center gap-3">
               <i className="fa-solid fa-heart text-primary text-xl"></i>
@@ -110,6 +78,53 @@ export default function ({ slug }) {
           </div>
         </div>
       </div>
+    )
+  }
+
+  const FavouriteRecipes = () => {
+    return (
+      <div className="flex flex-col gap-5 p-10 xl:px-20 w-full">
+
+      </div>
+    )
+  }
+
+  return (
+    <div className="flex flex-col md:flex-row w-full h-fit">
+      {/* Side navigation bar for computer screen size */}
+      <div className="hidden md:flex flex-col py-2 min-w-48 w-1/5 bg-primary text-white">
+        {navSection.map((section, index) => (
+          <>
+            <div className={`flex flex-row items-center gap-2 px-6 py-3 hover:bg-secondary hover:cursor-pointer ${selectSection.name === section.name ? "bg-secondary" : ""}`} onClick={() => setSelectSection(section)}>
+              <i className={`fa-solid ${section.icon} text-lg`}></i>
+              <h3 className="text-xl">{section.name}</h3>
+            </div>
+            <hr className="border-white w-11/12 self-center"></hr>
+          </>
+        ))}
+      </div>
+      {/* Top navigation bar for mobile screen size */}
+      <div className="md:hidden flex flex-row justify-between items-center bg-secondary text-white w-full h-12 px-6">
+        <div className={`flex flex-row items-center gap-2`}>
+          <i className={`fa-solid ${selectSection.icon} text-md`}></i>
+          <h3 className="text-lg">{selectSection.name}</h3>
+        </div>
+        <i className={`fa-solid ${showSection ? "fa-chevron-up" : "fa-chevron-down"} text-lg hover:cursor-pointer`} onClick={() => setShowSection(showSection == false)}></i>
+      </div>
+      <div className={`md:hidden flex flex-col bg-primary text-white w-full h-fit ${showSection ? "flex" : "hidden"}`}>
+        {navSection.map((section, index) => (
+          <>
+            <div className={`flex flex-row items-center gap-2 px-6 py-3 hover:bg-secondary hover:cursor-pointer`} onClick={() => {setSelectSection(section); setShowSection(false)}}>
+              <i className={`fa-solid ${section.icon} text-md`}></i>
+              <h3 className="text-lg">{section.name}</h3>
+            </div>
+            <hr className="border-white w-11/12 self-center"></hr>
+          </>
+        ))}
+      </div>
+      {/* Content displayed */}
+      {selectSection.name === navSection[0]["name"] && <Profile />}
+      {selectSection.name === navSection[1]["name"] && <FavouriteRecipes />}
     </div>
   )
 }
